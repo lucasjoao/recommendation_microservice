8efb570957ec96441bd89ae6f78c3e76bac487ab
lucas <lucasjoao.lj@gmail.com>
2018-05-13 19:51:47 -0300

    start dialog component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 import {DataScroller} from 'primereact/components/datascroller/DataScroller';
 import {Button} from 'primereact/components/button/Button';
+import {Dialog} from 'primereact/components/dialog/Dialog';
 
 import './App.css';
 
 class App extends Component {
   state = {
-    users: []
+    users: [],
+    selectedUser: undefined,
+    visible: false
   }
 
   componentDidMount() {
@@ -17,6 +20,10 @@ class App extends Component {
       .then(({users}) => {
         this.setState({users: users})
       })
+  }
+
+  onHide(event) {
+    this.setState({visible: false})
   }
 
   userTemplate(user) {
@@ -30,7 +37,7 @@ class App extends Component {
           {user.name}
         </div>
         <div>
-          <Button label="Recomendação de amigos" onClick={this.onClick} />
+          <Button label="Recomendação de amigos" onClick={(e) => this.setState({selectedUser: user, visible: true})} />
         </div>
       </div>
     );
@@ -40,6 +47,9 @@ class App extends Component {
     return (
       <div className="App">
         <DataScroller value={this.state.users} itemTemplate={this.userTemplate.bind(this)} rows={10} header="Clique para ver as recomendações"/>
+        <Dialog header="Amigos sugeridos" visible={this.state.visible} modal={true} onHide={this.onHide}>
+          Teste
+        </Dialog>
       </div>
     );
   }
