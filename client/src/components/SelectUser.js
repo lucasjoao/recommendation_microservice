d8dae66a4b3360a243de1442e499a9c4bdbaba16
lucas <lucasjoao.lj@gmail.com>
2018-06-10 02:19:26 -0300

    finish selectUser page

--- client/src/components/SelectUser.js
+++ client/src/components/SelectUser.js
@@ -2,18 +2,45 @@ import React, { Component } from 'react';
 
 import {Button} from 'primereact/components/button/Button';
 import {InputText} from 'primereact/components/inputtext/InputText';
-
-// TODO: vai ser necessario usar propTypes?
+import Recommendations from './Recommendations';
 
 class SelectUser extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      user_id: '',
+      buttonClicked: false
+    }
+  }
+
+  nextPage() {
+    return (
+      <Recommendations type={this.state.user_id}/>
+    );
+  }
+
+  search() {
     return (
       <div className="block-center">
         <h3>Fill with the user's id</h3>
         <div className="ui-inputgroup">
-          <Button label="Search"/>
-          <InputText placeholder="User id" type="text"/>
+          <Button label="Search"
+                  onClick={() => this.setState({buttonClicked: true})}/>
+
+          <InputText placeholder="User id" type="text"
+                     onChange={(e) =>
+                                this.setState({user_id: e.target.value})}/>
         </div>
+      </div>
+    );
+  }
+
+  render() {
+    let content = (this.state.buttonClicked ? this.nextPage() : this.search());
+
+    return (
+      <div>
+        {content}
       </div>
     )
   }
